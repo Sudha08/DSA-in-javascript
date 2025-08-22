dd422bf55aa735d4644ff3be5cbc9f773294622e
Sudha08 <sudha0894@gmail.com>
2025-08-22 17:36:39 +0530

    intial commit

---
+++ Lesson1-Intro.js
@@ -0,0 +1,33 @@
+// Lesson 1 - Introduction
+//-----------------------------
+
+console.log("Lesson 1 - Introduction");
+
+/* ------------------------------------------------------------- */
+
+// Sum of 2 numbers
+
+function sum (a , b) {
+    let res = a + b;
+    return res;
+}
+
+console.log("Sum" + " - " + sum (10, 20));
+
+let x = 30, y = 50;
+console.log("Sum of "+ x + " + " + y +  " = " + sum (x, y));
+
+/* ------------------------------------------------------------- */
+
+// Square of 2 nums
+
+function calculateSquare (num) {
+    let square = num * num;
+    return square;
+}
+
+let n = 5;
+let squareRes = calculateSquare(n);
+console.log("Square of " + n + " - " + squareRes);
+
+/* ------------------------------------------------------------- */
---
+++ Lesson4-Secondlargest.js
@@ -0,0 +1,18 @@
+function secondLargest(arr) {
+    if (arr.length < 2) {
+        return null;
+    }
+    
+    let firstLargest = -Infinity;
+    let secondLargest = -Infinity;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > firstLargest) {
+            secondLargest = firstLargest;
+            firstLargest = arr[i];
+        } else if (arr[i] > secondLargest && arr[i] !== firstLargest) {
+            secondLargest = arr[i];
+        }
+    }
+    return secondLargest;
+}
