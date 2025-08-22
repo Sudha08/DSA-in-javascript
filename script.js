a49e868aa68bebd3765f9a350bb412c7bd5f0f6d
Sudha08 <sudha0894@gmail.com>
2025-08-22 23:52:46 +0530

    code additions - index.html, scripts

---
+++ script.js
@@ -0,0 +1,20 @@
+showLesson('1');
+      
+function showLesson(num) {
+fetch("Lesson" + num + ".js")  // path to your JS file
+    .then(response => response.text())
+    .then(code => {
+    document.getElementById("code-content").textContent = code;
+    });
+}
+
+function openNav() {
+    document.getElementById("sidenav").style.width = "250px";
+    document.getElementById("content").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+    document.getElementById("content").style.marginLeft= "0";
+    
+}
