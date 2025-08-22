71d579e7df7580b714ccfb1bfcf9d70df15c3def
Sudha08 <sudha0894@gmail.com>
2025-08-23 01:02:12 +0530

    Style, module changes

---
+++ lessons.js
@@ -0,0 +1,4 @@
+import './Lesson1.js';
+import './Lesson2.js';
+import './Lesson3.js';
+import './Lesson4.js';
--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-import './Lesson1.js';
-import './Lesson2.js';
-import './Lesson3.js';
-import './Lesson4.js';
-
-
 showLesson('1');
       
 function showLesson(num) {
